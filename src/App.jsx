d108c81197be09f51b0c3e04e8fc6f6d44f0a27e
AdnaneTaleb10 <adnane102464@gmail.com>
2025-08-24 12:15:06 +0100

    setup react project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Civilian</h1>
+      <h3>Create enchanting CVs</h3>
+    </>
+  )
+}
+
+export default App
