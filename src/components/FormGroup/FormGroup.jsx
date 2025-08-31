a29eb87370ed569b306a338d0117389a5eb594a7
AdnaneTaleb10 <adnane102464@gmail.com>
2025-08-31 11:14:04 +0100

    feat: add reusable FormGroup component with floating label

---
+++ src/components/FormGroup/FormGroup.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./FormGroup.css";
+
+export default function FormGroup({
+  isInput = true,
+  id,
+  labelName,
+  type = "text",
+  placeholder = "",
+  isRequired = true,
+}) {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleValueChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className="form-group">
+      <label className={inputValue ? "active" : ""} htmlFor={id}>
+        {labelName}
+      </label>
+
+      {isInput ? (
+        <input
+          id={id}
+          name={id}
+          type={type}
+          placeholder={placeholder}
+          required={isRequired}
+          onChange={handleValueChange}
+        />
+      ) : (
+        <textarea
+          id={id}
+          name={id}
+          placeholder={placeholder}
+          required={isRequired}
+          onChange={handleValueChange}
+        />
+      )}
+    </div>
+  );
+}
